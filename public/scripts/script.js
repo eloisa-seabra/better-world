c827cee5d6dda99b588b64e0b6168f0de7ddcfad
Eloisa Seabra <seabra.eloisa@gmail.com>
2020-12-27 16:00:01 +0000

    auto-reload-10sec

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,7 +6,8 @@ function closeNav() {
   document.getElementById('myNav').style.width = '0%';
 }
 /* set time out each 10 seconds
-/*
+ */
+
 window.setTimeout(function () {
   window.location.reload();
-}, 10000); */
+}, 10000);
